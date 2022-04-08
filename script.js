2581be53975da9a6a8ec5b886b453e43d0f2c474
emmegray <lunaienuso@libero.it>
2022-04-08 15:47:53 +0200

    Milestone 2

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 for (let index = 1; index < 100; index++) {
     if (index % 3 === 0 && index % 5 === 0) {
-        console.log("FizzBuzz");
+        scrivi("FizzBuzz");
     } else if (index % 3 === 0) {
-        console.log("Fizz");
+        scrivi("Fizz");
     } else if (index % 5 === 0) {
-        console.log("Buzz");
+        scrivi("Buzz");
     } else {
-        console.log(index);
+        scrivi(index);
     }
+}
+
+function scrivi(testo) {
+    document.getElementById("container").innerHTML = document.getElementById("container").innerHTML + "<li>"+testo+"</li>"
 }
