5b0af10e681ec61a12f2f8ddb02e83b3c678a38d
emmegray <lunaienuso@libero.it>
2022-04-08 16:18:52 +0200

    Fix numbers did not go up to 100

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-for (let index = 1; index < 100; index++) {
+for (let index = 1; index <= 100; index++) {
     if (index % 3 === 0 && index % 5 === 0) {
         scrivi("FizzBuzz");
     } else if (index % 3 === 0) {
